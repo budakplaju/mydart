80063208496ac912ae8e88fb5d9085bed4c1bc35
budakplaju <113337962+budakplaju@users.noreply.github.com>
2026-02-26 12:12:04 +0700

    Add files via upload

--- per1/flutter_bootstrap.js
+++ per1/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2064347094"
+    serviceWorkerVersion: "2587129390"
   }
 });
